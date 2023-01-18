bef736027cd5ab9f77df0d472f8c8824d9014dc6
jessseee <jssvssr2000@gmail.com>
2023-01-18 12:36:21 +0100

    Made begin_/end_datetime datetime-local fields in deployment form

--- label_studio/sensormodel/forms.py
+++ label_studio/sensormodel/forms.py
@@ -2,55 +2,62 @@ from django import forms
 from . import models
 from .models import Deployment
 
+
 class SensorForm(forms.ModelForm):
     class Meta:
         model = models.Sensor
-        fields = ['sensor_id','description','sensortype']
+        fields = ['sensor_id', 'description', 'sensortype']
+
 
 class SubjectForm(forms.ModelForm):
     class Meta:
         model = models.Subject
-        fields = ['name','color','size','extra_info']
+        fields = ['name', 'color', 'size', 'extra_info']
+
 
 class DeploymentForm(forms.ModelForm):
     class Meta:
         model = models.Deployment
-        fields = ['name','begin_datetime','end_datetime','location','sensor','subject']
+        fields = ['name', 'begin_datetime', 'end_datetime', 'location', 'sensor', 'subject']
+        widgets = {
+            'begin_datetime': forms.DateTimeInput(
+                attrs={'style': 'display: block; margin-bottom: 0.5em;', 'type': 'datetime-local', 'step': 1}),
+            'end_datetime': forms.DateTimeInput(
+                attrs={'style': 'display: block;  margin-bottom: 0.5em;', 'type': 'datetime-local', 'step': 1})
+        }
 
     def clean(self):
         cleaned_data = super(DeploymentForm, self).clean()
-         
+
         begin_datetime = cleaned_data.get('begin_datetime')
         end_datetime = cleaned_data.get('end_datetime')
 
         if begin_datetime and end_datetime:
-            if  begin_datetime >= end_datetime:
-                self.add_error('begin_datetime','Begin date time must be before end date time.')
+            if begin_datetime >= end_datetime:
+                self.add_error('begin_datetime', 'Begin date time must be before end date time.')
 
         sensors = cleaned_data.get('sensor')
         deployments = Deployment.objects.all()
 
-        overlap_begin, overlapping_deployment_begin  = False, []
+        overlap_begin, overlapping_deployment_begin = False, []
         overlap_end, overlapping_deployment_end = False, []
 
         for sensor in sensors:
             for deployment in deployments:
                 if sensor in deployment.sensor.all():
-                    if deployment.begin_datetime >= begin_datetime and deployment.begin_datetime <= end_datetime:
+                    if begin_datetime <= deployment.begin_datetime <= end_datetime:
                         overlapping_deployment_begin.append(str(deployment))
                         overlap_begin = True
-                    if deployment.end_datetime >= begin_datetime and deployment.end_datetime <= end_datetime:
+                    if begin_datetime <= deployment.end_datetime <= end_datetime:
                         overlapping_deployment_end.append(str(deployment))
-                        overlap_end = True                   
-
+                        overlap_end = True
 
         if overlap_begin:
             depl_list = ' '.join(overlapping_deployment_begin)
-            self.add_error('begin_datetime','Begin datetime overlaps with deployment(s): ' + depl_list)
+            self.add_error('begin_datetime', 'Begin datetime overlaps with deployment(s): ' + depl_list)
 
         if overlap_end:
             depl_list = ' '.join(overlapping_deployment_end)
-            self.add_error('end_datetime','End datetime overlaps with deployment(s): ' + depl_list)
+            self.add_error('end_datetime', 'End datetime overlaps with deployment(s): ' + depl_list)
 
-          
         return cleaned_data
