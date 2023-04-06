3640ecb296f835f12952e81e4b66d155a5e171e3
EriktHove <j.h.tenhove@student.utwente.nl>
2023-04-06 13:40:38 +0200

    split sensormodel

--- label_studio/sensordata/forms.py
+++ label_studio/sensordata/forms.py
@@ -6,3 +6,9 @@ class SensorDataForm(forms.Form):
     name = forms.CharField(max_length=100, required=False)
     sensor = forms.ModelChoiceField(sensormodelmodels.Sensor.objects.all())
     file = forms.FileField()
+
+class SensorOffsetForm(forms.Form):
+    sensor_1 = forms.ModelChoiceField(sensormodelmodels.Sensor.objects.all())
+    sensor_2 = forms.ModelChoiceField(sensormodelmodels.Sensor.objects.all())
+    offset = forms.IntegerField()
+    offset_date = forms.DateTimeField()
